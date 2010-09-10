666ccd112c6992cfda973429f90b23e7a6a0ff91
tim.hutton <tim.hutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2010-09-10 11:18:51 +0000

    ADD: Schlogl model

---
+++ Schlogl/schlogl.cpp
@@ -0,0 +1,110 @@
+/*
+
+A port of part of Greg Turk's reaction-diffusion code, from:
+http://www.cc.gatech.edu/~turk/reaction_diffusion/reaction_diffusion.html
+
+See README.txt for more details.
+
+*/
+
+// stdlib:
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+// local:
+#include "defs.h"
+#include "display.h"
+
+void init(float a[X][Y]);
+
+void compute(float a[X][Y],
+             float da[X][Y],
+             float speed);
+
+int main()
+{
+    // Schlogl model:
+    // F. Schlogl, "Chemical reaction models for non-equilibrium phase transitions", Zeitschrift fur Physik, 253, 147, (1972) 
+    // Following:
+    // http://wwwnlds.physik.tu-berlin.de/~hizanidis/talks/front_propagation_noise.pdf
+
+    // -- parameters --
+    float speed = 0.1f;
+    // ----------------
+    
+    // this array stores the chemical concentrations:
+    float a[X][Y];
+    // this array store the rate of change of those chemicals:
+    float da[X][Y];
+
+    // put the initial conditions into each cell
+    init(a);
+    
+    int iteration = 0;
+    while(true) 
+    {
+        // compute:
+        compute(a,da,speed);
+
+        // display:
+        if(iteration%10==0) 
+        {
+            if(display(a,a,a,iteration,true,200.0f,2.0f,10,"Voting (Esc to quit)")) // did user ask to quit?
+                break;
+        }
+
+        iteration++;
+    }
+}
+
+// return a random value between lower and upper
+float frand(float lower,float upper)
+{
+    return lower + rand()*(upper-lower)/RAND_MAX;
+}
+
+void init(float a[X][Y])
+{
+    srand((unsigned int)time(NULL));
+    
+    // figure the values
+    for(int i = 0; i < X; i++) {
+        for(int j = 0; j < Y; j++) {
+            a[i][j] = frand(-1.0f,1.0f);
+        }
+    }
+}
+
+void compute(float a[X][Y],
+             float da[X][Y],
+             float speed)
+{
+    // compute change in each cell
+    for(int i = 0; i < X; i++) 
+    {
+        int iprev = (i + X - 1) % X;
+        int inext = (i + 1) % X;
+
+        for(int j = 0; j < Y; j++) 
+        {
+            int jprev = (j + Y - 1) % Y;
+            int jnext = (j + 1) % Y;
+
+            float aval = a[i][j];
+
+            // compute the Laplacian of a
+            float dda = a[i][jprev] + a[i][jnext] + a[iprev][j] + a[inext][j] - 4*aval;
+
+            // compute the new rate of change of a
+            da[i][j] = dda + aval*(1-aval)*(1+aval); // a-a^3
+        }
+    }
+
+    // effect change
+    for(int i = 0; i < X; i++) {
+        for(int j = 0; j < Y; j++) {
+            a[i][j] += (speed * da[i][j]);
+        }
+    }
+}
