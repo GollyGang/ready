eef58cd0906ee6d8a3a085dc4950280dd9a3b94a
tim.hutton <tim.hutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2011-11-28 21:43:22 +0000

    NEW: a sketch of a possible GUI framework, using wxWidgets and VTK

---
+++ ReaDy_gui/app.hpp
@@ -0,0 +1,31 @@
+/*  Copyright 2011, The ReaDy Bunch
+
+    This file is part of ReaDy.
+
+    ReaDy is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ReaDy is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with ReaDy. If not, see <http://www.gnu.org/licenses/>.         */
+
+// wxWidgets:
+#include "wx/wxprec.h"
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
