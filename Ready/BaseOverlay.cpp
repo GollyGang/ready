2e58f141d92d82f5bc43df08d223fcc12a29d1f7
tim.hutton <tim.hutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2012-01-24 18:40:35 +0000

    ADD: added an 'overlay' idea: that we can describe starting patterns as a stack of overlays

---
+++ Ready/BaseOverlay.cpp
@@ -0,0 +1,43 @@
+/*  Copyright 2011, 2012 The Ready Bunch
+
+    This file is part of Ready.
+
+    Ready is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ready is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ready. If not, see <http://www.gnu.org/licenses/>.         */
+
+// local:
+#include "BaseOverlay.hpp"
+#include "utils.hpp"
+
+// STL:
+#include <stdexcept>
+using namespace std;
+
+void RectangleOverlay::Apply(int iChemical,const PointND& at,float& value) const
+{
+    if(!at.InRect(this->corner1,this->corner2)) return; // leave points outside untouched
+    
+    float f;
+    switch(this->fill_mode)
+    {
+        case Constant: f = this->value1; break;
+        case WhiteNoise: f = frand(this->value1,this->value2); break;
+        default: throw runtime_error("RectangleOverlay::Apply : Unsupported fill mode");
+    }
+    switch(this->paste_mode)
+    {
+        case Copy: value = f; break;
+        case Add: value += f; break;
+        default: throw runtime_error("RectangleOverlay::Apply : Unsupported paste mode");
+    }
+}
