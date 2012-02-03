96540cc6ac2f3ea040368451012766a842771de8
timhutton <timhutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2012-02-03 16:57:38 +0000

    ENH: minimal example that shows wxLongStringProperty issue on GTK: can't quit while editing the long string

--- wxTestbed/main.cpp
+++ wxTestbed/main.cpp
@@ -1,4 +1,3 @@
-// wxWidgets:
 #include <wx/wxprec.h>
 #ifdef __BORLANDC__
     #pragma hdrstop
@@ -6,18 +5,13 @@
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
-#include <wx/aui/aui.h>
+#include <wx/propgrid/propgrid.h>
 
 class MyFrame : public wxFrame
 {
     public:
 
         MyFrame(const wxString& title);
-        ~MyFrame();
-
-    private:
-
-        wxAuiManager aui_mgr;
 };
 
 class MyApp : public wxApp
@@ -27,7 +21,7 @@ class MyApp : public wxApp
         virtual bool OnInit();
 };
 
-DECLARE_APP(MyApp)   // so other files can use wxGetApp
+DECLARE_APP(MyApp)
 
 IMPLEMENT_APP(MyApp)
 
@@ -36,8 +30,6 @@ bool MyApp::OnInit()
     if ( !wxApp::OnInit() )
         return false;
 
-    SetAppName(_("wxTestbed"));
-
     MyFrame *frame = new MyFrame(_("wxTestbed"));
     SetTopWindow(frame);
     frame->Show();
@@ -45,38 +37,14 @@ bool MyApp::OnInit()
     return true;
 }
 
-// frame constructor
 MyFrame::MyFrame(const wxString& title)
        : wxFrame(NULL, wxID_ANY, title)
 {
-    this->aui_mgr.SetManagedWindow(this);
-
-    this->aui_mgr.AddPane(new wxTextCtrl(this,wxID_ANY),
-              wxAuiPaneInfo()
-              .Name("pane1")
-              .Caption(_("Pane 1"))
-              .BestSize(500,300)
-              .CenterPane()
-              );
-
-    this->aui_mgr.AddPane(new wxTextCtrl(this,wxID_ANY),
-              wxAuiPaneInfo()
-              .Name("pane2")
-              .Caption(_("Pane 2"))
-              .BestSize(500,300)
-              );
-
-    this->aui_mgr.AddPane(new wxTextCtrl(this,wxID_ANY),
-              wxAuiPaneInfo()
-              .Name("pane3")
-              .Caption(_("Pane 3"))
-              .BestSize(500,300)
-              );
-
-    this->aui_mgr.Update();
-}
-
-MyFrame::~MyFrame()
-{
-    this->aui_mgr.UnInit();
+    wxPropertyGrid *pg = new wxPropertyGrid(this,wxID_ANY);
+    pg->Append(new wxStringProperty(_("string:"),wxPG_LABEL,_("blah")));
+    pg->Append(new wxLongStringProperty(_("long string:"),wxPG_LABEL,_("blah blah blah")));
+    
+    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
+    sizer->Add(pg,wxSizerFlags(1).Expand());
+    this->SetSizer(sizer);
 }
