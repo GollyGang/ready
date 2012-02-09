2b97e52717226e401f43c96991a51111e6f1569e
tim.hutton <tim.hutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2012-02-09 10:22:16 +0000

    ADD: new full-kernel implementation, and a test pattern for it (kernel_test.vti) ADD: simple monospace message box in wxutils (for kernel errors) REN: renamed OpenCL_nDim to OpenCL_Formula

---
+++ trunk/Ready/OpenCL_FullKernel.hpp
@@ -0,0 +1,38 @@
+/*  Copyright 2011, 2012 The Ready Bunch
+
+    This file is part of Ready.
+
+    Ready is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ready is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ready. If not, see <http://www.gnu.org/licenses/>.         */
+
+// local:
+#include "OpenCL_RD.hpp"
+
+// n-dimensional (1D,2D,3D) OpenCL RD implementations with n chemicals, specified as
+// a full OpenCL kernel, for maximum flexibility
+class OpenCL_FullKernel : public OpenCL_RD
+{
+    public:
+
+        OpenCL_FullKernel();
+
+        virtual int GetBlockSizeX() const { return this->block_size[0]; }
+        virtual int GetBlockSizeY() const { return this->block_size[1]; }
+        virtual int GetBlockSizeZ() const { return this->block_size[2]; }
+
+        virtual std::string AssembleKernelSourceFromFormula(std::string formula) const;
+        
+    protected:
+    
+        int block_size[3];
+};
