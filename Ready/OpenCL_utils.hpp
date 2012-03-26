bcae4e3733b412d780874e28d9508c9099387d3f
andrew@trevorrow.com <andrew@trevorrow.com@27e6d130-d1e8-fefa-e648-5d177f96e310>
2012-03-26 22:42:55 +0000

    Added CL_* defines missing from Apple's OpenCL headers.

--- Ready/OpenCL_utils.hpp
+++ Ready/OpenCL_utils.hpp
@@ -19,6 +19,18 @@
 #ifdef __APPLE__
     // OpenCL is linked at start up time on Mac OS 10.6+
     #include <OpenCL/opencl.h>
+    // we need these defs from OpenCL_Dyn_Load.h because they
+    // are not in the OpenCL headers in Mac OS 10.6
+    #define CL_DEVICE_PREFERRED_VECTOR_WIDTH_HALF       0x1034
+    #define CL_DEVICE_HOST_UNIFIED_MEMORY               0x1035
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_CHAR          0x1036
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_SHORT         0x1037
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_INT           0x1038
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_LONG          0x1039
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_FLOAT         0x103A
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_DOUBLE        0x103B
+    #define CL_DEVICE_NATIVE_VECTOR_WIDTH_HALF          0x103C
+    #define CL_DEVICE_OPENCL_C_VERSION                  0x103D
 #else
     // OpenCL is loaded dynamically on Windows and Linux
     #include "OpenCL_Dyn_Load.h"
