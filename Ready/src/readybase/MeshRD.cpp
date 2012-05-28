2b5e4b6b9ce265f245856c81aeb19c92cc3e2098
tim.hutton <tim.hutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2012-05-28 16:53:46 +0000

    FIX: better tolerance for changing the number of chemicals

--- Ready/src/readybase/MeshRD.cpp
+++ Ready/src/readybase/MeshRD.cpp
@@ -172,7 +172,8 @@ void MeshRD::GenerateInitialPattern()
 
             int iC = overlay->GetTargetChemical();
             if(iC<0 || iC>=this->GetNumberOfChemicals())
-                throw runtime_error("Overlay: chemical out of range: "+GetChemicalName(iC));
+                continue; // best for now to silently ignore this overlay, because the user has no way of editing the overlays (short of editing the file)
+                //throw runtime_error("Overlay: chemical out of range: "+GetChemicalName(iC));
 
             float val;
             vector<float> vals(this->GetNumberOfChemicals());
