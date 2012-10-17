0ed205e6a9f656f6d1e1964ea2d1756cf204f17b
tim.hutton <tim.hutton@27e6d130-d1e8-fefa-e648-5d177f96e310>
2012-10-17 17:14:00 +0000

    ENH: simplified display of running speed to timesteps per second and an estimation of what proportion of time is spent rendering

--- trunk/Ready/src/gui/frame.hpp
+++ trunk/Ready/src/gui/frame.hpp
@@ -227,11 +227,13 @@ class MyFrame : public wxFrame, public IPaintHandler
 
         // following are used when running a simulation:
         bool is_running;
-        double frames_per_second, million_cell_generations_per_second;
         int num_steps;
         int steps_since_last_render;
-        double accumulated_time;
+        double computation_time_since_last_render, computed_frames_per_second_buffer[10];
+        double time_at_last_render, percentage_spent_rendering;
+        double smoothed_timesteps_per_second,timesteps_per_second_buffer[10];
         bool do_one_render;
+        int i_timesteps_per_second_buffer;
 
         // used when recording frames to disk
         bool is_recording,record_data_image;
