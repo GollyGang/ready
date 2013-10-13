61e0cd317b10280676f1034aae30022e11114c8e
tim.hutton@gmail.com <tim.hutton@gmail.com@27e6d130-d1e8-fefa-e648-5d177f96e310>
2013-10-13 22:34:10 +0000

    FIX: missing source files

---
+++ Ready/src/readybase/scene_items.cpp
@@ -0,0 +1,37 @@
+/*  Copyright 2011, 2012 The Ready Bunch
+
+    This file is part of Ready.
+
+    Ready is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ready is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ready. If not, see <http://www.gnu.org/licenses/>.         */
+    
+// local:
+#include "scene_items.hpp"
+
+// VTK:
+#include <vtkRenderer.h>
+#include <vtkScalarsToColors.h>
+#include <vtkScalarBarActor.h>
+#include <vtkSmartPointer.h>
+
+void AddScalarBar(vtkRenderer* pRenderer,vtkScalarsToColors* lut)
+{
+    vtkSmartPointer<vtkScalarBarActor> scalar_bar = vtkSmartPointer<vtkScalarBarActor>::New();
+    scalar_bar->SetLookupTable(lut);
+    scalar_bar->SetOrientationToHorizontal();
+    scalar_bar->SetWidth(0.5);
+    scalar_bar->SetHeight(0.15);
+    scalar_bar->SetPosition(0.25,0.01);
+    scalar_bar->SetMaximumHeightInPixels(80);
+    pRenderer->AddActor2D(scalar_bar);
+}
