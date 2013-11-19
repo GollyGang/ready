524cd1752bb2bf04037e57b3f1d081f676ac6f26
tim.hutton@gmail.com <tim.hutton@gmail.com@27e6d130-d1e8-fefa-e648-5d177f96e310>
2013-11-19 00:06:41 +0000

    BRANCH: Made a branch for 0.6, for any later bug fixing on that release.

--- SpeedComparisons/GrayScott_HWIVector/display_hwiv.h
+++
@@ -1,3 +0,0 @@
-bool display(int width, int height, float *r, float *g, float *b,
-             double iteration, float model_scale, bool auto_brighten,float manual_brighten,
-			 int scale,int delay_ms,const char* message, bool write_video);
